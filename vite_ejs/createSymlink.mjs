5530c40a4162ccb9c0311fef66cd09e4f75c79fe
KOBAYASHI Eiji <eiji@design24c.com>
2024-05-21 16:49:29 +0900

    createSymlink.mjs ファイルを追加

---
+++ vite_ejs/createSymlink.mjs
@@ -0,0 +1,27 @@
+import { symlink, mkdir } from 'fs';
+import { resolve, dirname } from 'path';
+
+// シンボリックリンクを作成する関数
+function createSymlink(target, linkPath) {
+  symlink(target, linkPath, 'junction', (err) => {
+    if (err) {
+      console.error('Error creating symlink:', err);
+    } else {
+      console.log('Symlink created successfully:', linkPath);
+    }
+  });
+}
+
+// 実行するパスを指定
+const targetPath = '../../../.temp/public/assets/images';
+const linkPath = 'src/public/assets/images';
+
+// ディレクトリが存在しない場合は作成
+mkdir(dirname(linkPath), { recursive: true }, (err) => {
+  if (err) {
+    console.error('Error creating directory:', err);
+  } else {
+    // シンボリックリンクを作成
+    createSymlink(targetPath, linkPath);
+  }
+});
